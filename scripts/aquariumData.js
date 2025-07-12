fc68032e2b8b6a731a4c1a82fd9e49dc165c1f00
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-07-12 12:47:46 -0500

    Added data to database

---
+++ scripts/aquariumData.js
@@ -0,0 +1,60 @@
+export const database = {
+    fish: [
+        {
+            name: "Nemo",
+            species: "Clownfish",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-162235445-1.jpg"
+        },
+        {
+            name: "Dory",
+            species: "Blue Tang",
+            length: 5,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-462249793.jpg"
+        },
+        {
+            name: "Peach",
+            species: "Starfish",
+            length: 12,
+            location: "Indian Ocean" ,
+            diet: "Omnivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-182213653.jpg"
+        },
+        {
+            name: "Sheldon",
+            species: "Seahorse",
+            length: 7,
+            location: "Great Barrier Reef",
+            diet: "Carnivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-518089168.jpg" 
+        },
+        {
+            name: "Tad",
+            species: "Butterflyfish",
+            length: 7,
+            location: "Great Barrier Reef",
+            diet: "Corallivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-1210604845.jpg" 
+        },
+        {
+            name: "Mr. Ray",
+            species: "Spotted Eagle Ray",
+            length: 60,
+            location: "Atlantic Ocean",
+            diet: "Carnivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-1399039144.jpg"
+        },
+        {
+            name: "Gill",
+            species: "Moorish Idol",
+            length: 8,
+            location: "Indian & Pacific Ocean",
+            diet: "Omnivore",
+            image: "https://www.bristolaquarium.co.uk/wp-content/uploads/2023/06/iStock-154948410.jpg"
+        }
+    ]
+}

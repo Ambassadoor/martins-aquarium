f1355b9a882edba25f56d7466923e2cb878d2ff6
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-07-12 16:53:52 -0500

    Added styling

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -2,18 +2,28 @@ import { database } from './aquariumData.js';
 
 export const fishList = () => {
     // Generate an HTML representation of each fish
-    let htmlString = `<article class="fishList">`
+    let htmlString = `
+    <article class="fishList">
+    <div id = "fishHeader">
+        <h2>Fish</h2>
+    </div>
+    <div id="fishCardsBox">`
+    
 
     database.fish.map(fish => {
-        htmlString += `<section class="fish card">
-            <div><img  class="fish__image image--card" src="${fish.image}" /></div>
-            <div class="fish__name">Name: ${fish.name}</div>
-            <div class="fish__species">Species: ${fish.species}</div>
-            <div class="fish__length">Length: ${fish.length} inches</div>
-            <div class="fish__location">Location: ${fish.location}</div>
+        htmlString += `<section class="fishCard">
+            <div class="fishText">
+                <p class="fish__name">Name: ${fish.name}</p>
+                <p class="fish__species">Species: ${fish.species}</p>
+                <p class="fish__length">Length: ${fish.length} inches</p>
+                <p class="fish__location">Location: ${fish.location}</p>
+            </div>
+            <div class="fishImageBox">
+                <img  class="fishImage" src="${fish.image}" />
+            </div>
         </section>
 `
-    htmlString += `</article>`
     })
+        htmlString += `</div></article>`
     return htmlString
 };
--- scripts/locationList.js
+++ scripts/locationList.js
@@ -1,12 +1,21 @@
 import { database } from "./aquariumData.js";
 
 export const locationList = () => {
-    let html = `<article class="locations">`
+    let html = `<article class="locations">
+    <div id = "locationsHeader">
+    <h2>Locations</h2>
+    </div>
+    <div id ="locationsCardsBox">`
     database.locations.map(location => {
-        html += `<section class="location">${location.name}
+        html += `<section class="location">
+            <div class="locationsText">
+                <h3>${location.name}</h3>
+                <p>Countries: ${location.country}</p>
+                <p>${location.description}</p>
+            </div>
         </section>`
     })
 
-    html += `</article>`
+    html += `</div></article>`
     return html
 }
